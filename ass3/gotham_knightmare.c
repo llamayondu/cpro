297add39dec6ecf44eb4f8ae876927bde75555e3
Shreeprabhas <shreeprabhas@gmail.com>
2023-10-10 21:59:56 +0530

    finally working

--- ass3/gotham_knightmare.c
+++ ass3/gotham_knightmare.c
@@ -1,81 +1,53 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
+#include <stdlib.h>
+#define BLOCKED -1
 
-#define MOD 1000000007
+int n, m, k;
 
 int main(){
-    int n, m, k;
     scanf("%d %d %d", &n, &m, &k);
+    long long int gotham[n][m+4];
+    
+    memset(gotham, 0, n*(m+4)*(sizeof(long long int)));
 
-    int blocks[k][2];
-    for(int i=0; i<k; i++){
-        scanf("%d %d", &blocks[i][0], &blocks[i][1]);
-    }
-
-    for(int i=0; i<k; i++){
-        blocks[i][0]=n+1-blocks[i][0];
-    }
-
-    int gotham_sim[3][m+4][m];
-    memset(gotham_sim, 0, 3*m*(m+4)*sizeof(int));
-    for(int i=0; i<m; i++){
-        gotham_sim[0][i+2][i]++;
-    }
-    // for(int i=0; i<m; i++){
-    //     if(i>=2){
-    //         gotham_sim[1][i][i]++;
-    //     }
-    //     if(i+4<=m-1){
-    //         gotham_sim[1][i][i+4]++;
+    // for(int i=0; i<n; i++){
+    //     for(int j=0; j<m+4; j++){
+    //         gotham[i][j]=0;
     //     }
     // }
 
-    for(int i=2; i<m+2; i++){
-        if(i-2>=2){
-            gotham_sim[1][i][i-4]++;
-        }
-        if(i+2<=m+1){
-            gotham_sim[1][i][i]++;
-        }
+    for(int i=0; i<k; i++){
+        int tempx, tempy;
+        scanf("%d %d", &tempx, &tempy);
+        gotham[tempx-1][tempy+1]=BLOCKED;
     }
 
-    for(int t=0; t<k; t++){
-        if(blocks[t][0]==2){
-            memset(gotham_sim[1][blocks[t][1]+1], 0, m*sizeof(int));
-        }
+    for(int i=2; i<m+2; i++){
+        gotham[0][i]=1;
+    }
+
+    if(gotham[1][2]!=BLOCKED&&m>=3) gotham[1][2]=1;
+    if(gotham[1][3]!=BLOCKED&&m>=4) gotham[1][3]=1;
+    if(gotham[1][m]!=BLOCKED&&m-4>=0) gotham[1][m]=1;
+    if(gotham[1][m+1]!=BLOCKED&&m-3>=0) gotham[1][m+1]=1;
+
+    for(int i=4; i<m; i++){
+        if(gotham[1][i]!=BLOCKED) gotham[1][i]=2;
     }
 
     for(int i=2; i<n; i++){
-        memset(gotham_sim[i%3], 0, (m+4)*m*sizeof(int));
         for(int j=2; j<m+2; j++){
-            for(int k=0; k<m; k++){
-                gotham_sim[i%3][j][k]+=gotham_sim[(i-1)%3][j-2][k];//wide left
-                gotham_sim[i%3][j][k]+=gotham_sim[(i-2)%3][j-1][k];//tall left
-                gotham_sim[i%3][j][k]+=gotham_sim[(i-2)%3][j+1][k];//tall right
-                gotham_sim[i%3][j][k]+=gotham_sim[(i-1)%3][j+2][k];//wide right
-                gotham_sim[i%3][j][k]%=MOD;
-            }
-        }
-        for(int t=0; t<k; t++){
-            if(i==blocks[t][0]-1){
-                memset(gotham_sim[i%3][blocks[t][1]+1], 0, m*sizeof(int));
-            }
+            if(gotham[i][j]==BLOCKED) continue;
+            if(gotham[i-1][j-2]!=BLOCKED) gotham[i][j] += gotham[i-1][j-2];
+            if(gotham[i-2][j-1]!=BLOCKED) gotham[i][j] += gotham[i-2][j-1];
+            if(gotham[i-2][j+1]!=BLOCKED) gotham[i][j] += gotham[i-2][j+1];
+            if(gotham[i-1][j+2]!=BLOCKED) gotham[i][j] += gotham[i-1][j+2];
+            gotham[i][j]%=1000000007;
         }
     }
 
-    int final_sum[m];
-    for(int i=0; i<m; i++){
-        final_sum[i]=0;
-    }
-
-    for(int j=2; j<m+2; j++){
-        for(int k=0; k<m; k++){
-            final_sum[k]+=gotham_sim[(n-1)%3][j][k];
-        }
-    }
-
-    for(int i=0; i<m; i++){
-        printf("%d ", final_sum[i]%MOD);
+    for(int i=2; i<m+2; i++){
+        printf("%lld ", gotham[n-1][i]%1000000007);
     }
 }
